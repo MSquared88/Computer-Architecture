bc53c5b3ed3cad31ba61e885aca118d87be1c3e2
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2020-03-26 17:53:35 -0500

    refactured for branch table

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,19 +2,18 @@
 
 import sys
 
-op_codes = {
-    'HLT':  0b00000001,
-    'LDI':  0b10000010,
-    'PRN':  0b01000111,
-    'PUSH': 0b01000101,
-    'POP':  0b01000110,
-    'CALL': 0b01010000,
-    'RET':  0b00010001,
+HLT =  0b00000001
+LDI =  0b10000010
+PRN =  0b01000111
+PUSH = 0b01000101
+POP =  0b01000110
+CALL = 0b01010000
+RET =  0b00010001
 
-    # alu opcodes
-    "ADD":  0b10100000,
-    'MUL':  0b10100010,
-}
+# alu opcodes
+ADD =  0b10100000
+MUL =  0b10100010
+
 
 class CPU:
     """Main CPU class."""
@@ -25,6 +24,22 @@ class CPU:
         self.sp = 7
         self.reg = [0] * 8
         self.ram = [0] * 256 
+
+        #setup branchtable
+        self.branchtable = {}
+        self.branchtable[LDI] = self.handle_LDI
+        self.branchtable[PRN] = self.handle_PRN
+        self.branchtable[PUSH] = self.handle_PUSH
+        self.branchtable[POP] = self.handle_POP
+        self.branchtable[CALL] = self.handle_CALL
+        self.branchtable[RET] = self.handle_RET
+
+
+        #alu 
+        self.branchtable[ADD] = self.handle_LDI
+        self.branchtable[MUL] = self.handle_LDI
+
+
     def ram_read(self, address):
         return self.ram[address]
 
@@ -87,63 +102,62 @@ class CPU:
 
         print()
 
+
+    def handle_LDI(self, a=None, b=None):
+        self.reg[a] = b
+        self.pc += 3
+
+    def handle_PRN(self, a=None, b=None):
+        print(self.reg[a])
+        self.pc += 2
+
+    def handle_PUSH(self, a=None, b=None):
+        self.reg[self.sp] -= 1
+        self.ram[self.reg[self.sp]] = self.reg[a]
+        self.pc += 2
+
+    def handle_POP(self, a=None, b=None):
+        memory = self.ram[self.reg[self.sp]]
+        self.reg[a] = memory
+        self.reg[self.sp] += 1
+        self.pc += 2
+
+    def handle_CALL(self, a=None, b=None):
+        # The address of the ***instruction*** _directly after_ `CALL` is
+        # pushed onto the stack.
+        self.reg[self.sp] -= 1
+        self.ram[self.reg[self.sp]] = self.pc + 2
+        # The PC is set to the address stored in the given register.
+        self.pc = self.reg[a]
+
+    def handle_RET(self, a=None, b=None):
+        self.pc = self.ram[self.reg[self.sp]]
+        self.reg[self.sp] += 1
+
     def run(self):
         """Run the CPU."""
         self.reg = [0] * 8
         self.reg[self.sp] = 0xf3
 
+
         while True:
             IR = self.ram[self.pc]
+            if IR == HLT:
+                sys.exit()
+                break
+            elif IR not in self.branchtable:
+                print(f'Unknown instruction: {IR}')
+                sys.exit(1)
 
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
 
-            if IR == op_codes['HLT']:
-                sys.exit()
-                break
-            
-            elif IR == op_codes['LDI']:
-                self.reg[operand_a] = operand_b
-                self.pc += 3
-
-            elif IR == op_codes['PRN']:
-                print(self.reg[operand_a])
-                self.pc += 2
-
-            elif IR == op_codes['MUL']:
+            if IR == MUL:
                 self.alu('MUL', operand_a, operand_b)
                 self.pc += 3
-            elif IR == op_codes['ADD']:
+            elif IR == ADD:
                 self.alu('ADD', operand_a, operand_b)
                 self.pc += 3
 
-            elif IR == op_codes['PUSH']:
-
-                self.reg[self.sp] -= 1
-                self.ram[self.reg[self.sp]] = self.reg[operand_a]
-                self.pc += 2
-
-
-            elif IR == op_codes['POP']:
-                memory = self.ram[self.reg[self.sp]]
-                self.reg[operand_a] = memory
-                self.reg[self.sp] += 1
-                self.pc += 2
-            
-            elif IR == op_codes['CALL']:
-                # The address of the ***instruction*** _directly after_ `CALL` is
-                # pushed onto the stack.
-                self.reg[self.sp] -= 1
-                self.ram[self.reg[self.sp]] = self.pc + 2
-                # The PC is set to the address stored in the given register.
-                self.pc = self.reg[operand_a]
-
-                # We jump to that location in RAM and execute the first instruction in the subroutine.
-                # The PC can move forward or backwards from its current location.
-            elif IR == op_codes['RET']:
-                self.pc = self.ram[self.reg[self.sp]]
-                self.reg[self.sp] += 1
-
             else:
-                print(f'Unknown instruction: {IR}')
-                sys.exit(1)
+                self.branchtable[IR](operand_a, operand_b)
